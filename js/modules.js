1f0f2a95285627e1a35bbb025bd93b260069d151
hellobasti <bastianwinterott@gmail.com>
2016-11-12 23:11:33 +0100

    Deleted javascript files in folder js

--- js/modules.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const textblock_1 = require("./textblock");
-exports.Textblock = textblock_1.Textblock;
-const textblockRequest_1 = require("./textblockRequest");
-exports.TextblockRequest = textblockRequest_1.TextblockRequest;
