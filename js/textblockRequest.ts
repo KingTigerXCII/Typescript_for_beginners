204cdfbcc1db56a8b35ced3e5e6ef68a01cd4462
hellobasti <bastianwinterott@gmail.com>
2016-11-24 20:54:56 +0100

    module loading works fine

--- js/textblockRequest.ts
+++ js/textblockRequest.ts
@@ -2,26 +2,18 @@ import { Textblock } from "./modules";
 
 export class TextblockRequest {
 
-    constructor() {
-
-        console.log("Request Textblocks");
-
-    }
-
     public createTextblock(numberOfTextblocks: number) {
 
         console.log("Request Textblocks");
         let arrayOfTextblocks: Textblock[] = new Array(); 
 
-        /*
+        
         for(let i = 0; i < numberOfTextblocks; i++)
         {
-
+            let indexOfArray = arrayOfTextblocks.push(new Textblock("I'm a textblock " + i, "My name is ......."));
+            console.log(indexOfArray); 
+            document.body.innerHTML += arrayOfTextblocks[indexOfArray-1].getTextblockWithCSSClass();
         }
-        */
-
-        let indexOfArray = arrayOfTextblocks.push(new Textblock("I'm a textblock ", "My name is .......")); 
-        document.body.innerHTML += arrayOfTextblocks[0].getTextblockWithCSSClass();
 
     }
 
